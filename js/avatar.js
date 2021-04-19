3ab3ce83d440344f2f1ac27222f12994bba01d07
cyckl <cyckl@pm.me>
2021-04-19 01:47:42 -0700

    Slight JS optimization

--- js/avatar.js
+++ js/avatar.js
@@ -1,5 +1,5 @@
 // Take in Discord ID from html tag and fetch user profile picture from Discord
 
-document.querySelectorAll(".avatar").forEach(avatar => fetch(`https://nixne.st/avatar/` + avatar.getAttribute("data-user-id")).then(r => r.text()).then(url => avatar.src = url));
+document.querySelectorAll(".avatar").forEach(avatar => fetch(`https://nixne.st/avatar/${avatar.getAttribute("data-user-id")}`).then(r => r.text()).then(url => avatar.src = url));
 
 // vib chonk
